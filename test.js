22be074460c6f0bf5e0e42f5cca2294b7d704015
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-11-09 16:45:13 +0100

    ex_2 fix validator

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-let score = "";
-let a = 1; let b = 7;
-for (let i = b; i >= a; i--) {
-    score += i + " ";
+const validator = (str) => {
+    if (str === "")
+        return false;
+    for (i in str) {
+        if (str[i] === "-" && i === "0")  
+            continue;
+        if (isNaN(parseInt(str[i])))
+            return false;
+    }
+    return true;
 }
-console.log(score);
+
+
+console.log(validator("-90"));
